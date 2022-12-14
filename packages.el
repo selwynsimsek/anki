f47d229085f79b09f6476ce2bd17e30b69d31ed7
Selwyn Simsek <selwyn.simsek@quantinuum.com>
2022-12-14 11:41:44 +0000

    Add Anki packages

--- packages.el
+++ packages.el
@@ -41,30 +41,29 @@
 ;;; Code:
 
 (defconst anki-packages
-  '()
-  "The list of Lisp packages required by the anki layer.
+  '(anki-connect
+    anki-editor
+    anki-mode
+    anki-vocabulary
+    org-anki))
 
-Each entry is either:
+  (defun anki/init-anki-connect ()
+    (use-package anki-connect))
 
-1. A symbol, which is interpreted as a package to be installed, or
 
-2. A list of the form (PACKAGE KEYS...), where PACKAGE is the
-    name of the package to be installed or loaded, and KEYS are
-    any number of keyword-value-pairs.
+  (defun anki/init-anki-editor ()
+    (use-package anki-editor
+      :after org-noter
+      :config (setq anki-editor-create-decks 't)))
 
-    The following keys are accepted:
 
-    - :excluded (t or nil): Prevent the package from being loaded
-      if value is non-nil
+  (defun anki/init-anki-mode ()
+    (use-package anki-mode))
 
-    - :location: Specify a custom installation location.
-      The following values are legal:
 
-      - The symbol `elpa' (default) means PACKAGE will be
-        installed using the Emacs package manager.
+  (defun anki/init-anki-vocabulary ()
+    (use-package anki-vocabulary))
 
-      - The symbol `local' directs Spacemacs to load the file at
-        `./local/PACKAGE/PACKAGE.el'
 
-      - A list beginning with the symbol `recipe' is a melpa
-        recipe.  See: https://github.com/milkypostman/melpa#recipe-format")
+  (defun anki/init-org-anki ()
+    (use-package org-anki))
